246f685da191792410c202ed4e47a223502e1434
Leos Julien <leos.julien@gmail.com>
2020-02-20 20:21:38 +0100

    parsingde

---
+++ core.py
@@ -0,0 +1,10 @@
+class Core:
+    def __init__(self, booksNumber, librariesNumber, daysNumber, booksScore, librairies):
+        self.booksNumber = booksNumber
+        self.librariesNumber = librariesNumber
+        self.daysNumber = daysNumber
+        self.booksScore = booksScore
+        self.librairies = librairies
+
+    def setScore(self, library):
+        print("toto")
