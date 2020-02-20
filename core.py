e4d53ac77597a8fcb2fa28030101db402a31f483
Leos Julien <leos.julien@gmail.com>
2020-02-20 20:56:43 +0100

    score

--- core.py
+++ core.py
@@ -1,10 +1,37 @@
+import math
+
 class Core:
     def __init__(self, booksNumber, librariesNumber, daysNumber, booksScore, librairies):
-        self.booksNumber = booksNumber
-        self.librariesNumber = librariesNumber
-        self.daysNumber = daysNumber
-        self.booksScore = booksScore
-        self.librairies = librairies
+        self.booksNumber = booksNumber #Number
+        self.librariesNumber = librariesNumber #Number
+        self.daysNumber = daysNumber #Number
+        self.booksScore = booksScore #List of Number
+        self.librairies = librairies #List of Object
+
+        self.actualDay = 0
+        self.end = False
+
+        self.gameLoop()
+
+    def getBooksSortedByScore(self, books):
+        booksScore = [self.booksScore[index] for index in books]
+        response = [x for _,x in sorted(zip(booksScore, books))]
+        response.reverse()
+        return response
 
     def setScore(self, library):
-        print("toto")
+        sortedBooks = self.getBooksSortedByScore(library["books"])
+        daysAvailable = (self.daysNumber - self.actualDay) - library["signUpDays"]
+        numberBooksGettable = min(library["booksNumber"], daysAvailable * library["shipCapacity"])
+        daysTaken = library["signUpDays"] + math.ceil(numberBooksGettable / library["shipCapacity"])
+        
+        scores = [self.booksScore[index] for index in sortedBooks]
+        score = sum(scores[:numberBooksGettable]) / daysTaken
+        library["score"] = score
+
+    def gameLoop(self):
+        while not self.end:
+            for library in self.librairies:
+                self.setScore(library)
+            self.actualDay += 1
+            self.end = True
--- parsing.py
+++ parsing.py
@@ -29,15 +29,15 @@ class Parsing:
     def parse(self):
         lines = self.rawData.split("\n")
         lines = [string for string in lines if string != ""]
-        [self.booksNumber, self.librariesNumber, self.daysNumber] = lines[0].split(" ")
+        [self.booksNumber, self.librariesNumber, self.daysNumber] = list(map(int, lines[0].split(" ")))
         self.booksScore = list(map(int, lines[1].split(" ")))
         lines = lines[2:]
         for index in range(0, len(lines), 2):
             libraryInfo = lines[index].split(" ")
             self.libraries.append({
-                "booksNumber": libraryInfo[0],
-                "signUpDays": libraryInfo[1],
-                "shipCapacity": libraryInfo[2],
+                "booksNumber": int(libraryInfo[0]),
+                "signUpDays": int(libraryInfo[1]),
+                "shipCapacity": int(libraryInfo[2]),
                 "books": list(map(int, lines[index + 1].split(" ")))
             })
 
